58d6b73c82a08540e39eeeba24367490ca285c18
kolar083 <nidzokolarevic@gmail.com>
2026-01-16 02:10:30 +0100

    Dodat novi prozor

--- client/app.js
+++ client/app.js
@@ -58,3 +58,20 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+window.onload = function() {
+  const modal = document.getElementById("myModal");
+  const span = document.getElementsByClassName("close")[0];
+
+  modal.style.display = "block";
+
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+};
